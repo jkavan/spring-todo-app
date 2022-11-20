75ac0b6b890622c565783c3e0d98b9c5cb789422
jkavan <jani@kavander.fi>
2022-11-20 20:11:02 +0200

    Remove unused imports

--- src/main/java/jkavan/todo/web/ProjectController.java
+++ src/main/java/jkavan/todo/web/ProjectController.java
@@ -1,8 +1,5 @@
 package jkavan.todo.web;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -11,18 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import jkavan.todo.domain.Project;
 import jkavan.todo.domain.ProjectRepository;
-import jkavan.todo.domain.Task;
-import jkavan.todo.domain.TaskRepository;
 
 @Controller
 public class ProjectController {
-
-  @Autowired
-  private TaskRepository tasks;
 
   @Autowired
   private ProjectRepository repository;
--- src/main/java/jkavan/todo/web/TaskController.java
+++ src/main/java/jkavan/todo/web/TaskController.java
@@ -1,7 +1,6 @@
 package jkavan.todo.web;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import jkavan.todo.domain.Task;
 import jkavan.todo.domain.TaskRepository;
