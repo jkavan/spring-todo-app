6801fefe6ee5a6014ab7ae754ea43d139aec4005
jkavan <jani@kavander.fi>
2022-11-20 20:29:31 +0200

    Add tests for controllers

--- src/test/java/jkavan/todo/TodoApplicationTests.java
+++ src/test/java/jkavan/todo/TodoApplicationTests.java
@@ -1,13 +1,27 @@
 package jkavan.todo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import jkavan.todo.web.ProjectController;
+import jkavan.todo.web.TaskController;
 
 @SpringBootTest
 class TodoApplicationTests {
-
+  
+  @Autowired
+  private TaskController taskController;
+  
+  @Autowired
+  private ProjectController projectController;
+  
   @Test
   void contextLoads() {
+    assertThat(taskController).isNotNull();
+    assertThat(projectController).isNotNull();
   }
 
 }
